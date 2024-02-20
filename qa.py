f50180922e56800e819400aef7a74d61521e0eca
kip <kkipngenokoech22@gmail.com>
2024-02-20 22:07:50 +0300

    Add qa.py and prompts.cpython-310.pyc files

---
+++ qa.py
@@ -0,0 +1,11 @@
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.llms import OpenAI
+from prompts import chat
+
+prompt = PromptTemplate(template="Question: {question}\nAnswer:", input_variables=["question"])
+
+# llm = OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0)
+chain = LLMChain(llm=chat, prompt=prompt)
+
+chain.run(question="What is AgriAid?")
